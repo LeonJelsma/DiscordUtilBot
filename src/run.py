3e9c7fe9aa750b1a787ea1cd53e41c74bf336813
Leon <leonjelsma@gmail.com>
2020-09-04 18:23:10 +0200

    changes

--- src/run.py
+++ src/run.py
@@ -5,8 +5,13 @@ import src.const as const
 from src.config import Config
 from os.path import join as join
 
+
+startup_extensions = ["audio"]
 bot = commands.Bot(command_prefix='!', description="Minge")
+bot.volume = 0.2
+
 config = Config()
+
 
 @bot.event
 async def on_ready():
@@ -15,6 +20,20 @@ async def on_ready():
             f'{bot.user} is connected to the following guild:\n'
             f'{guild.name}(id: {guild.id})\n'
         )
+
+
+@bot.event
+async def on_voice_state_update(member, before, after):
+    if before.channel is None:
+        test = bot
+        for client in bot.voice_clients:
+            if client.channel == after.channel:
+                audio = discord.FFmpegPCMAudio(
+                    executable="C:\\Users\\Leon\\PycharmProjects\MingeBotTwo\\resources\\ffmpeg\\bin\\ffmpeg.exe",
+                    source="C:\\Users\\Leon\\PycharmProjects\\MingeBotTwo\\resources\\audio\\memes\\hellothere.wav")
+                audio = discord.PCMVolumeTransformer(audio, bot.volume)
+                client.channel.play(audio)
+
 
 
 @bot.command(name="join")
@@ -28,22 +47,31 @@ async def leave(ctx):
     await ctx.voice_client.disconnect()
 
 
+@bot.command(name="volume")
+async def volume(ctx, _volume: int):
+    bot.volume = float(_volume / 100)
+
+
 @bot.command(name="audiotest")
 async def audiotest(ctx):
-    voice_channel = ctx.author.channel
+    voice_channel = ctx.author.voice.channel
     channel = None
     if voice_channel is not None:
         channel = voice_channel.name
         vc = await voice_channel.connect()
-        vc.play(discord.FFmpegPCMAudio(executable=const.FFMPEG_DIR, source=join(const.AUDIO_MEMES_DIR, 'dejavu.wav')))
+
+        audio = discord.FFmpegPCMAudio(executable="C:\\Users\\Leon\\PycharmProjects\MingeBotTwo\\resources\\ffmpeg\\bin\\ffmpeg.exe", source="C:\\Users\\Leon\\PycharmProjects\\MingeBotTwo\\resources\\audio\\memes\\dejavu.wav")
+        audio = discord.PCMVolumeTransformer(audio, bot.volume)
+        vc.play(audio)
         # Sleep while audio is playing.
         while vc.is_playing():
-            sleep(.1)
+            
+            await sleep(0.1)
         await vc.disconnect()
     else:
         await ctx.send(str(ctx.author.name) + "is not in a channel.")
     # Delete command after the audio is done playing.
-    await ctx.message.delete()
+    # await ctx.message.delete()
 
 
 try:
